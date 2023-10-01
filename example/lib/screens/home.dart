33b596b4f9f0f5df8e0e525ab4c211f786967b02
Gautier de Lataillade <32983806+gdelataillade@users.noreply.github.com>
2023-10-01 18:04:47 +0200

    Increment plugin version to 2.1.0

--- example/lib/screens/home.dart
+++ example/lib/screens/home.dart
@@ -71,7 +71,7 @@ class _ExampleAlarmHomeScreenState extends State<ExampleAlarmHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('alarm 2.0.1')),
+      appBar: AppBar(title: const Text('alarm 2.1.0')),
       body: SafeArea(
         child: alarms.isNotEmpty
             ? ListView.separated(
