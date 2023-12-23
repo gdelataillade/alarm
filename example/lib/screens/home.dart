bba00f347314ce90b1ec739f73114c48f508a88e
Gautier de Lataillade <32983806+gdelataillade@users.noreply.github.com>
2023-12-23 15:50:33 +0100

    Update plugin version in example app AppBar

--- example/lib/screens/home.dart
+++ example/lib/screens/home.dart
@@ -98,7 +98,7 @@ class _ExampleAlarmHomeScreenState extends State<ExampleAlarmHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('alarm 3.0.1')),
+      appBar: AppBar(title: const Text('alarm 3.0.2')),
       body: SafeArea(
         child: alarms.isNotEmpty
             ? ListView.separated(
