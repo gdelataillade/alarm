ae4e1dd2dec578316b01652a883f586663d7bf1c
Gautier de Lataillade <32983806+gdelataillade@users.noreply.github.com>
2024-01-12 17:46:18 +0100

    Fix vibrations for Android below API 26

--- android/src/main/kotlin/com/gdelataillade/alarm/services/VibrationService.kt
+++ android/src/main/kotlin/com/gdelataillade/alarm/services/VibrationService.kt
@@ -9,8 +9,14 @@ class VibrationService(private val context: Context) {
     private val vibrator: Vibrator = context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
     fun startVibrating(pattern: LongArray, repeat: Int) {
-        val vibrationEffect = VibrationEffect.createWaveform(pattern, repeat)
-        vibrator.vibrate(vibrationEffect)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Use VibrationEffect for API 26 and above
+            val vibrationEffect = VibrationEffect.createWaveform(pattern, repeat)
+            vibrator.vibrate(vibrationEffect)
+        } else {
+            // Use the older vibrate method for below API 26
+            vibrator.vibrate(pattern, repeat)
+        }
     }
 
     fun stopVibrating() {
