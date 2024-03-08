8ed64f6d9274241d9553182361958d3917b9a9ad
Nhật Long <116779782+longtn-imt@users.noreply.github.com>
2024-03-08 06:45:02 +0000

    fix(iOS): SwiftAlarmPlugin.scheduleAppRefresh() EXC_BAD_ACCESS

--- ios/Classes/SwiftAlarmPlugin.swift
+++ ios/Classes/SwiftAlarmPlugin.swift
@@ -148,8 +148,8 @@ public class SwiftAlarmPlugin: NSObject, FlutterPlugin {
 
                 DispatchQueue.main.async {
                     self.timers[id] = Timer.scheduledTimer(timeInterval: delayInSeconds, target: self, selector: #selector(self.executeTask(_:)), userInfo: id, repeats: false)
-                    SwiftAlarmPlugin.scheduleAppRefresh()
                 }
+                SwiftAlarmPlugin.scheduleAppRefresh()
             }
             result(true)
         } else {
