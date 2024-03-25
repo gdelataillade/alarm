e20a3fff40c4d23b6c06d839e53f73696069001b
Gautier de Lataillade <gautier2406@gmail.com>
2024-03-26 00:20:19 +0100

    Reschedule iOS scheduled alarms when time changes

--- ios/Classes/services/NotificationManager.swift
+++ ios/Classes/services/NotificationManager.swift
@@ -6,22 +6,40 @@ class NotificationManager {
 
     private init() {} // Private initializer to ensure singleton usage
 
-    func requestAuthorization(completion: @escaping (Bool, Error?) -> Void) {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: completion)
+    // Checks and requests authorization to show notifications
+    func ensureAuthorization(completion: @escaping (Bool, Error?) -> Void) {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            switch settings.authorizationStatus {
+            case .authorized, .provisional:
+                completion(true, nil)
+            case .notDetermined:
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: completion)
+            case .denied:
+                completion(false, nil)
+            @unknown default:
+                completion(false, nil)
+            }
+        }
     }
 
+    // Creates notification content
+    private func createContent(title: String, body: String) -> UNMutableNotificationContent {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.sound = nil
+        return content
+    }
+
+    // Schedules a notification to be triggered after a delay
     func scheduleNotification(id: String, delayInSeconds: Int, title: String, body: String, completion: @escaping (Error?) -> Void) {
-        requestAuthorization { granted, error in
-            guard granted, error == nil else {
+        ensureAuthorization { [weak self] granted, error in
+            guard let self = self, granted, error == nil else {
                 completion(error)
                 return
             }
             
-            let content = UNMutableNotificationContent()
-            content.title = title
-            content.body = body
-            content.sound = nil
-
+            let content = self.createContent(title: title, body: body)
             let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(delayInSeconds), repeats: false)
             let request = UNNotificationRequest(identifier: "alarm-\(id)", content: content, trigger: trigger)
 
@@ -29,6 +47,22 @@ class NotificationManager {
         }
     }
 
+    // Triggers a notification immediately
+    func triggerNotification(id: String, title: String, body: String, completion: @escaping (Error?) -> Void) {
+        ensureAuthorization { [weak self] granted, error in
+            guard let self = self, granted, error == nil else {
+                completion(error)
+                return
+            }
+            
+            let content = self.createContent(title: title, body: body)
+            let request = UNNotificationRequest(identifier: "alarm-\(id)", content: content, trigger: nil)
+
+            UNUserNotificationCenter.current().add(request, withCompletionHandler: completion)
+        }
+    }
+
+    // Cancels a pending notification
     func cancelNotification(id: String) {
         UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["alarm-\(id)"])
     }
