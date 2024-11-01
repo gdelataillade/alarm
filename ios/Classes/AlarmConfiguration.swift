4f781cdbedf63f4fdfe499bb5f568e15518f3d0b
Gautier de Lataillade <gautier2406@gmail.com>
2024-11-01 19:04:22 +0100

    Add volume enforcement for iOS

--- ios/Classes/AlarmConfiguration.swift
+++ ios/Classes/AlarmConfiguration.swift
@@ -7,17 +7,20 @@ class AlarmConfiguration {
     let loopAudio: Bool
     let fadeDuration: Double
     let volume: Float?
+    var volumeEnforced: Bool
+    var volumeEnforcementTimer: Timer?
     var triggerTime: Date?
     var audioPlayer: AVAudioPlayer?
     var timer: Timer?
     var task: DispatchWorkItem?
 
-    init(id: Int, assetAudio: String, vibrationsEnabled: Bool, loopAudio: Bool, fadeDuration: Double, volume: Float?) {
+    init(id: Int, assetAudio: String, vibrationsEnabled: Bool, loopAudio: Bool, fadeDuration: Double, volume: Float?, volumeEnforced: Bool) {
         self.id = id
         self.assetAudio = assetAudio
         self.vibrationsEnabled = vibrationsEnabled
         self.loopAudio = loopAudio
         self.fadeDuration = fadeDuration
         self.volume = volume
+        self.volumeEnforced = volumeEnforced
     }
 }
