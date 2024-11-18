fe3940c7985a3f5ba496ebca2b054036eb96f986
Gautier de Lataillade <gautier2406@gmail.com>
2024-11-18 11:22:40 +0100

    Increment plugin version to 4.0.12

--- example/lib/screens/home.dart
+++ example/lib/screens/home.dart
@@ -9,7 +9,7 @@ import 'package:alarm_example/widgets/tile.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-const version = '4.0.11';
+const version = '4.0.12';
 
 class ExampleAlarmHomeScreen extends StatefulWidget {
   const ExampleAlarmHomeScreen({super.key});
