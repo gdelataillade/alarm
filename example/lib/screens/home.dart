35c195a93c64c340d1c5368c39a542ba19457ec0
Gautier de Lataillade <gautier2406@gmail.com>
2026-05-19 22:40:43 -0400

    Increment plugin version to 5.3.0

--- example/lib/screens/home.dart
+++ example/lib/screens/home.dart
@@ -11,7 +11,7 @@ import 'package:alarm_example/widgets/tile.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-const version = '5.2.1';
+const version = '5.3.0';
 
 class ExampleAlarmHomeScreen extends StatefulWidget {
   const ExampleAlarmHomeScreen({super.key});
